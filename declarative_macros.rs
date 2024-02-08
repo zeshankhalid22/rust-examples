c175fcb7b739c3bb8e3a49786afb84e4f86161e3
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-02-08 22:08:21 +0500

    added simple custom my_vec! (vec!)

--- declarative_macros.rs
+++ declarative_macros.rs
@@ -1,3 +1,24 @@
-fn main {
-// TODO learning declarative macros
+
+macro_rules! my_macro {
+    // , = one or more separated things
+    // ($element:expr) is the pattern where $element matches any expression and
+    // gives the founded expression the name element
+
+    ($ ($element:expr),* ) => {
+        {
+        let mut vec = Vec::new();
+        // $(___)* to give repetitions currosponding to the pattern
+        $(
+            vec.push($element);
+        )*
+        vec
+         }
+    }
+
+}
+
+
+fn main() {
+    let mv = my_macro!(1,3,6);
+    println!("{:?}", mv);
 }
