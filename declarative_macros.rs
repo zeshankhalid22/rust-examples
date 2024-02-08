5b3eb824f7f0b8bbb848450e4ac6049e87c7e6d5
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-02-08 22:51:36 +0500

    my_vec![elem; size] added

--- declarative_macros.rs
+++ declarative_macros.rs
@@ -1,10 +1,11 @@
-
-macro_rules! my_macro {
+macro_rules! my_vec {
     // , = one or more separated things
     // ($element:expr) is the pattern where $element matches any expression and
     // gives the founded expression the name element
 
+    // Block 1
     ($ ($element:expr),* ) => {
+        // ($elem:expr,) if commas are at the end of elem like [1,] [1,2,3,]
         {
         let mut vec = Vec::new();
         // $(___)* to give repetitions currosponding to the pattern
@@ -13,12 +14,24 @@ macro_rules! my_macro {
         )*
         vec
          }
-    }
+    };
 
+    // Block # 2 takes [element; count_of_how_many_times_to_repeat/push]
+    ($element:expr; $count:expr) => {{
+        let mut vs = Vec::with_capacity($count);
+        for _ in 0..$count {
+            vs.push($element);
+        }
+        // vs.resize($count, $element) // also work
+        vs
+    }};
 }
 
 
 fn main() {
-    let mv = my_macro!(1,3,6);
+
+    let mv = my_vec!(1,3,6);
     println!("{:?}", mv);
+    let mv2 = my_vec!(2;5);
+    println!("{:?}", mv2);
 }
